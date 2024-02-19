48dfb3a44c8540c605352dd3a952165acdc99306
loud3stsil3nce <rafiurrahman325@gmail.com>
2024-02-19 02:16:32 -0500

    Set up more variables and initial operate function. Currently does not function but I will fix soon.

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 const calculatorBody = document.querySelector('#calculator');
 
-function add(a , b) {
-    let sum = a + b;
+function add(num1 , num2) {
+    let sum = num1 + num2;
     return sum;
 }
 
-function subtract(a , b) {
-    let difference = a - b;
+function subtract(num1 , num2) {
+    let difference = num1 - num2;
     return difference;
 }
 
-function multiply(a , b) {
-    let product = a * b;
+function multiply(num1 , num2) {
+    let product = num1 * num2;
     return product;
 }
 
-function divide(a , b) {
-    let quotient = a / b;
+function divide(num1 , num2) {
+    let quotient = num1 / num2;
     return quotient;
 }
+
+
+
+let number1 = '';
+let number2 = '';
+let operator = '';
+
+
+
+function operate (operator, number1, number2) {
+    switch (operator) {
+    case '+':
+    return add(number1, number2);
+    break;
+    case '-':
+    return subtract(number1, number2);
+    break;
+    case '*':
+    return multiply(number1, number2);
+    break;
+    case '/':
+    return divide(number1, number2);
+    }
+    }
